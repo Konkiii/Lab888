7b7ab4c73ce5aaaa6924dabd583406c282d28a2c
Pattaraporn <pattaraporn_ruen@cmu.ac.th>
2021-12-03 18:33:33 +0700

    x

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,13 +1,26 @@
 #include<iostream>
 using namespace std;
 
-char before(char x){
-	//Write your function definition here
+int main()
+{
+	char x = 'B';
+	if(x = 65){
+		cout << 'Z';
+	}else if((x >= 65) and (x <= 90)){
+		x = x -1;
+		cout << x;
+	}else{
+		cout << '0';
+	}
+
 }
 
-int main(){
-	//Test Case
+/*int main(){
+	char x = 'A';
 	cout << before('A') << "\n";
+	*/
+
+	/*
 	cout << before('B') << "\n";
 	cout << before('P') << "\n";
 	cout << before('T') << "\n";
@@ -15,5 +28,6 @@ int main(){
 	cout << before('a') << "\n";
 	cout << before('0') << "\n";
 	cout << before('c') << "\n";
-	return 0;
-}
+*/
+	
+
